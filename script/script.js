7b77e4d935e3733edf425934d6aacca41c96e96a
Nayara Lima <nayaralima@poli.ufrj.br>
2022-06-29 17:52:49 -0300

    página 2 - get data

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 let quizz;
+let object;
 
 const urlApi = 'https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes';
 
@@ -30,7 +31,7 @@ function renderQuizz() {
     quizz.forEach(item => {
 
         template = /* html */`
-            <div class="quizz" onclick="startQuizz()">
+            <div class="quizz" id="${item.id}" onclick="startQuizz(this) ">
                 <img src="${item.image}" alt="">
                 <div class="title-quizz">
                     ${item.title}
@@ -43,11 +44,74 @@ function renderQuizz() {
 }
 
 // PASSO 4 - Inicia quizz (clicando no quizz adiciona o hidden ao section e box-user)
-function startQuizz() {
+function startQuizz(selectedQuizz) {
 
     const $quizz = document.querySelector('section');
     const $boxUser = document.querySelector('.box-user');
+    const $container = document.querySelector('.container')
+    const $quizPage = document.querySelector('.quiz-page')
 
     $quizz.classList.add('hidden');
     $boxUser.classList.add('hidden');
+    $container.classList.add('hidden');
+    $quizPage.classList.remove('hidden');
+
+    console.log(selectedQuizz.id);
+    
+    searchIndividualQuizz(selectedQuizz.id);
+}
+
+// PASSO 5 -  Faz requisição do quizz individual
+function searchIndividualQuizz(id) {
+
+    const promise = axios.get(`${urlApi}/${id}`);
+    promise.then(processIndividualQuizz);   
+}
+
+function processIndividualQuizz(response) {
+    object = response.data;
+    console.log(object);
+
+    renderIndividualQuizz()
+}
+
+// Passo 6 - Renderiza as tela do quiz individual com as respectivas perguntas
+function renderIndividualQuizz() {
+    
+    const quizPage = document.querySelector(".quiz-page");    
+
+    quizPage.innerHTML += `<div class="quiz-head" 
+                            style="background: linear-gradient(0deg, rgba(0, 0, 0, 0.57), rgba(0, 0, 0, 0.57)), 
+                            url(${object.image}) no-repeat; 
+                            background-size: cover">
+                            <h1>${object.title}</h1>
+                            </div>`;
+
+    let questions = object.questions;
+    
+
+    for (let i = 0; i < questions.length; i ++) {
+
+        quizPage.innerHTML+= ` <div class="question-box">
+                                    <div class="question"
+                                    style="background-color: ${questions[i].color} ">
+                                        <h2>${questions[i].title}</h2>
+                                    </div>
+                                    <div class="alternatives">
+                                    </div>
+                                            `;
+
+        let answers = questions[i].answers;
+        for (let j = 0; j < answers.length; j++) {
+
+            const questionBox = document.querySelectorAll(".question-box .alternatives");
+            questionBox[questionBox.length-1].innerHTML += `
+                                    <div >
+                                        <img src="${answers[j].image}">
+                                        <p>${answers[j].text}</p>
+                                    </div>
+                                            `;
+        };
+    };
+    
 }
