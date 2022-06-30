ccffd5349a5bd2f829f01b472e3f2c64cf120397
melissamodesto <mbm.modesto@gmail.com>
2022-06-30 01:00:49 -0300

    Quizz Levels

--- script/script.js
+++ script/script.js
@@ -31,7 +31,7 @@ function processQuizz(response) {
 
 
 function renderQuizz() {
-    
+
     const quizzElement = document.querySelector('.box-quizz');
 
     let template;
@@ -53,7 +53,7 @@ function renderQuizz() {
     })
 }
 
-// PASSO 4 - Inicia quizz (clicando no quizz adiciona o hidden ao section e box-user)
+// PASSO 4 - Inicia quizz 
 const $quizz = document.querySelector('section');
 const $boxUser = document.querySelector('.box-user');
 const $container = document.querySelector('.container');
@@ -68,7 +68,7 @@ function startQuizz(selectedQuizz) {
     $quizPage.classList.remove('hidden');
 
     console.log(selectedQuizz.id);
-    
+
     searchIndividualQuizz(selectedQuizz.id);
 }
 
@@ -76,7 +76,7 @@ function startQuizz(selectedQuizz) {
 function searchIndividualQuizz(id) {
 
     const promise = axios.get(`${urlApi}/${id}`);
-    promise.then(processIndividualQuizz);   
+    promise.then(processIndividualQuizz);
 }
 
 function processIndividualQuizz(response) {
@@ -88,8 +88,8 @@ function processIndividualQuizz(response) {
 
 // Passo 6 - Renderiza as tela do quiz individual com as respectivas perguntas
 function renderIndividualQuizz() {
-    
-    const quizPage = document.querySelector(".quiz-page");    
+
+    const quizPage = document.querySelector(".quiz-page");
 
     quizPage.innerHTML += `<div class="quiz-head" 
                             style="background: linear-gradient(0deg, rgba(0, 0, 0, 0.57), rgba(0, 0, 0, 0.57)), 
@@ -99,11 +99,11 @@ function renderIndividualQuizz() {
                             </div>`;
 
     let questions = object.questions;
-    
 
-    for (let i = 0; i < questions.length; i ++) {
 
-        quizPage.innerHTML+= ` <div class="question-box">
+    for (let i = 0; i < questions.length; i++) {
+
+        quizPage.innerHTML += ` <div class="question-box">
                                     <div class="question"
                                     style="background-color: ${questions[i].color} ">
                                         <h2>${questions[i].title}</h2>
@@ -113,14 +113,14 @@ function renderIndividualQuizz() {
                                             `;
 
         let answers = questions[i].answers;
-        answers.sort(comparador); 
-        function comparador() { 
-	        return Math.random() - 0.5; 
+        answers.sort(comparador);
+        function comparador() {
+            return Math.random() - 0.5;
         }
         for (let j = 0; j < answers.length; j++) {
 
             const questionBox = document.querySelectorAll(".question-box .alternatives");
-            questionBox[questionBox.length-1].innerHTML += `
+            questionBox[questionBox.length - 1].innerHTML += `
                                     <div >
                                         <img src="${answers[j].image}">
                                         <p>${answers[j].text}</p>
@@ -128,7 +128,7 @@ function renderIndividualQuizz() {
                                             `;
         };
     };
-    
+
 }
 
 // Passo 7 - Tela de criação: Informações básicas do quiz
@@ -142,12 +142,12 @@ function quizBasicInfo() {
 }
 
 function validations() {
-    
+
     let quizTitle = document.querySelector('input:first-child').value;
     let URLimg = document.querySelector('input:nth-child(2)').value;
     let nQuestions = document.querySelector('input:nth-child(3)').value;
     let nLevels = document.querySelector('input:nth-child(4)').value;
-    
+
     let quiztitleOK = (quizTitle.length > 19 && quizTitle.length < 66);
     let nQuestionsOK = (nQuestions > 4);
     let nLevelsOK = (nLevels > 1);
@@ -155,7 +155,7 @@ function validations() {
         if (URLimg != null && URLimg != '') {
             let regex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
             return regex.test(str);
-        }else{
+        } else {
             return false;
         };
     });
@@ -164,31 +164,74 @@ function validations() {
         title = quizTitle;
         image = URLimg;
     } else {
-        alert("Verifique as consições necessárias para criar o Quizz e tente novamente (:");
+        alert("Verifique as condições necessárias para criar o Quizz e tente novamente (:");
     }
 
+}
+
+function validationLevel() {
+
+    let valid = false;
+    let regex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+
+    const $inputTitle = document.querySelector('.quizz-form [data-title]');
+    const $inputNumber = document.querySelector('.quizz-form [data-number]');
+    const $inputUrl = document.querySelector('.quizz-form [data-url]');
+    const $inputDescription = document.querySelector('.quizz-form [data-description]');
+
+    if ($inputTitle.value.length >= 10 &&
+        Number($inputNumber.value) <= 100 &&
+        Number($inputNumber.value) > 0 &&
+        regex.test($inputUrl.value) === true &&
+        $inputDescription.value.length >= 30) {
+
+        valid = true;
+    }
+
+    return valid;
 }
 
 function quizzLevel() {
 
     $info.classList.add('hidden');
     $quizzLevel.classList.remove('hidden');
-    
+
 }
 
 function quizzReady() {
     
+    if (validationLevel() === true) {
+        
         $quizzLevel.classList.add('hidden');
         $quizzSuccess.classList.remove('hidden');
-        
+
+    } else {
+        alert('Verifique as condições necessárias para criar o Quizz e tente novamente (:')
+    }
 }
 
-
 function quizzHome() {
-
+    
     $quizzLevel.classList.add('hidden');
     $info.classList.add('hidden');
     $quizzSuccess.classList.add('hidden');
     $initialScreen.classList.remove('hidden');
     
 }
+
+//Habilita botão do form da tela 3.3
+const $button = document.querySelector('.quizz-button');
+
+document.querySelectorAll('.quizz-form input').forEach(input => {
+        
+    input.addEventListener('input', () => {
+        
+        if (validationLevel() === true) {
+            $button.classList.remove('disabled');
+
+        } else {
+            $button.classList.add('disabled');
+        }
+    })
+})
+
